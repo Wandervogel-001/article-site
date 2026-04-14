3bbfb58401773ad61163aa479b91ddbe85860880
Wandervogel-001 <youssefbouzidi727@gmail.com>
2026-04-14 20:16:13 +0100

    fixed some bugs

---
+++ src/content.config.ts
@@ -0,0 +1,20 @@
+import { defineCollection, z } from 'astro:content';
+import { glob } from 'astro/loaders';
+
+const articles = defineCollection({
+  loader: glob({
+    pattern: '**/*.md',
+    base: './src/content/articles',
+  }),
+
+  schema: z.object({
+    title: z.string(),
+    subtitle: z.string().optional(),
+    type: z.string().optional(),
+    date: z.coerce.date(),
+  }),
+});
+
+export const collections = {
+  articles,
+};
